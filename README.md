bef7537c5f55accdb593821c5f74b190f36d4c5f
tagedieb77 <lazee@gmx.net>
2011-12-14 04:16:34 +0100

    initial commit… adding core files

---
+++ README.md
@@ -0,0 +1,74 @@
+# regexposix #
+
+regexposix provides regular expression functions with POSIX character classes.
+
+## Methods ##
+
+**add** `regexposix.add(regex, identifier)`  
+  
+Adds a new character class [:identifier:] for the given regular expression.
+
+		regexposix.add(/[0-9]{4}/, 'fourdigits');
+		regexposix.test(/[:fourdigits:]/, 'foo2011bar')
+		// => true
+  
+**match** `regexposix.match(regex, string)`  
+
+Equals JavaScripts `RegExp.match()`.
+  
+**test** `regexposix.test(regex, string)`  
+
+Equals JavaScripts `RegExp.test()`.
+  
+**replace** `regexposix.replace(regex, string, replacement_string)`  
+
+Equals JavaScripts `RegExp.replace()`.
+  
+**replaceInArray** `regexposix.replaceInArray(regex, array, replacement_string)`  
+
+Similar to **replace** but for arrays.
+  
+		regexposix.replaceInArray(/[:digit:]+/, ['foo', 'bar', '2001', 'foo2011bar'], '-');
+		// => ['foo', 'bar', '-', 'foo-bar']
+
+**replaceCallback** `regexposix.replaceCallback(regex, array, function)`  
+
+This function is almost identical to **replace** but instead replacing the matches with a string they will be replaced by the result of the callback function.
+
+		regexposix.replaceCallback(/[:digit:]+/, 'foo2011bar, function(n) {
+			return Number(n)*2;
+		});
+		// => foo4022bar
+  
+**search** `regexposix.search(regex, string)`  
+
+Equals JavaScripts `RegExp.search()`.
+  
+**exec** `regexposix.exec(regex, string)`  
+
+Equals JavaScripts `RegExp.exec()`.
+
+## Predefined Character Classes ##
+
+		alnum:  "[A-Za-z0-9]",
+		alpha:  "[A-Za-z]",
+		blank:  "[ \\t]",
+		cntrl:  "[\\x00-\\x1F\\x7F]",
+		digit:  "[0-9]",
+		graph:  "[\\x21-\\x7E]",
+		lower:  "[a-z]",
+		print:  "[\\x20-\\x7E]",
+		punct:  "[!\"#$%&'()*+,\\-./:;<=>?@[\\\\\\]^_`{|}~]",
+		space:  "[ \\t\\r\\n\\v\\f]",
+		upper:  "[A-Z]",
+		word:   "[A-Za-z0-9_]",
+		xdigit: "[A-Fa-f0-9]",
+		nonspace:  "[^ \\t\\r\\n\\v\\f]",
+		nondigit:  "[^0-9]",
+		email: "(?:[\\w\\!\\#\\$\\%\\&\\'\\*\\+\\-\\/\\=\\?\\^\\`\\{\\|\\}\\~]+\\.)*[\\w\\!\\#\\$\\%\\&\\'\\*\\+\\-\\/\\=\\?\\^\\`\\{\\|\\}\\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\\-](?!\\.)){0,61}[a-zA-Z0-9]?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\\[(?:(?:[01]?\\d{1,2}|2[0-4]\\d|25[0-5])\\.){3}(?:[01]?\\d{1,2}|2[0-4]\\d|25[0-5])\\]))",
+		uuid: "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}"
+
+## License ##
+
+Released under the [MIT license](http://www.opensource.org/licenses/mit-license.php).
+
---
+++ regexposix.js
@@ -0,0 +1,100 @@
+(function(){
+  var root = this,
+  character_classes = {
+    alnum:  "[A-Za-z0-9]",
+    alpha:  "[A-Za-z]",
+    blank:  "[ \\t]",
+    cntrl:  "[\\x00-\\x1F\\x7F]",
+    digit:  "[0-9]",
+    graph:  "[\\x21-\\x7E]",
+    lower:  "[a-z]",
+    print:  "[\\x20-\\x7E]",
+    punct:  "[!\"#$%&'()*+,\\-./:;<=>?@[\\\\\\]^_`{|}~]",
+    space:  "[ \\t\\r\\n\\v\\f]",
+    upper:  "[A-Z]",
+    word:   "[A-Za-z0-9_]",
+    xdigit: "[A-Fa-f0-9]",
+    nonspace:  "[^ \\t\\r\\n\\v\\f]",
+    nondigit:  "[^0-9]",
+    email: "(?:[\\w\\!\\#\\$\\%\\&\\'\\*\\+\\-\\/\\=\\?\\^\\`\\{\\|\\}\\~]+\\.)*[\\w\\!\\#\\$\\%\\&\\'\\*\\+\\-\\/\\=\\?\\^\\`\\{\\|\\}\\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\\-](?!\\.)){0,61}[a-zA-Z0-9]?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\\[(?:(?:[01]?\\d{1,2}|2[0-4]\\d|25[0-5])\\.){3}(?:[01]?\\d{1,2}|2[0-4]\\d|25[0-5])\\]))",
+    uuid: "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}"
+  },
+  _regexToClassString = function(regex) {
+    return String(regex).replace(/^[\/]|[\/].*?$/g, '');
+  },
+  _regexToObject = function(regex) {
+    var rarr = String(regex).match(/^\/(.*?)\/([a-z]{0,10})$/);
+    var robj = {string: rarr[1], modifier: rarr[2]};
+    return robj;
+  },
+  _replacePosix = function(regex) {
+    var robj = _regexToObject(regex);
+    var posixfound = robj.string.match(/\[:([a-z\d]+):\]/ig);
+    for(var p in posixfound) {
+      robj.string = robj.string.replace(posixfound[p], character_classes[posixfound[p].replace(/[\[\]\:]/g,'')]);
+    }
+    return new RegExp(robj.string, robj.modifier);
+  };
+  rxp = {
+    VERSION: '0.1',
+    add: function(regex, id) {
+      character_classes[id] = _regexToClassString(regex);
+    },
+    match: function(regex, string) {
+      return String(string).match(_replacePosix(regex));
+    },
+    test: function(regex, string) {
+      return _replacePosix(regex).test(String(string)); 
+    },
+    replace: function(regex, string, replacement) {
+      if( typeof string === 'string' ) {
+        return String(string).replace(_replacePosix(regex), replacement); 
+      }
+      return string;
+    },
+    replaceInArray: function(regex, array, replacement) {
+      if( Object.prototype.toString.call(array) === '[object Array]' ) {
+        for( var i in array ) {
+          array[i] = String(array[i]).replace(_replacePosix(regex), replacement); 
+        }
+        return array;
+      }
+      return array;
+    },
+    replaceCallback: function(regex, string, cb) {
+      var ro = _regexToObject(regex);
+      var s = String(string).match(_replacePosix('/'+ro.string+'/'));
+      if( s ) {
+        var r = cb(s[0]);
+        regex = new RegExp(_regexToClassString(_replacePosix('/'+ro.string+'/')), ro.modifier);
+        return string.replace(regex, r);
+      }
+      return string;
+    },
+    exec: function(regex, string) {
+      return _replacePosix(regex).exec(String(str));
+    },
+    search: function(regex, string) {
+      return String(string).search(_replacePosix(regex));
+    }
+  };
+  
+  // Export the for **Node.js** and **"CommonJS"**, with
+  // backwards-compatibility for the old `require()` API. If we're not in
+  // CommonJS, add `regexposix` to the global object.
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = rxp;
+    }
+    exports = rxp;
+  } else if (typeof define === 'function' && define.amd) {
+    // Register as a named module with AMD.
+    define('regexposix', function() {
+      return rxp;
+    });
+  } else {
+    // Exported as a string, for Closure Compiler "advanced" mode.
+    root['regexposix'] = rxp;
+  }
+  
+})();
